615d7ba58336cc0d9920ba2ce964118769b43606
modmuss50 <modmuss50@gmail.com>
2018-11-11 13:00:44 +0000

    Add build file

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+echo 'Updating libs'
+go get -u github.com/modmuss50/dump1090-uploader
+
+rm -rf output
+mkdir output
+
+platforms=("windows/amd64" "windows/386" "linux/amd64" "linux/386" "linux/arm" "linux/arm64" "darwin/amd64")
+
+version="1.0.0"
+
+for platform in "${platforms[@]}"
+do
+    echo 'Building for ' ${platform}
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    output_name='output/dump1090-uploader-'$GOOS'-'$GOARCH'-'$version
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'
+    fi
+
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $package
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+done
